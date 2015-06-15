131b69563c63781eb91854357a50a3903c764b74
M. Edgar joel <edgar-@users.noreply.github.com>
2015-06-15 19:38:55 -0400

    form based workflow feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
+  root to: 'articles#index'
   resources :articles
   # See how all your routes lay out with "rake routes".
 
